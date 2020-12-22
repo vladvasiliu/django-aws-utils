18d73c4f9802b4073d3ed244f462e13b8e03f951
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2020-12-22 19:20:44 +0100

    Return the secret as-is.

--- django_aws_utils/secrets_manager.py
+++ django_aws_utils/secrets_manager.py
@@ -1,17 +1,18 @@
 import boto3
-import json
 from botocore.exceptions import ClientError
 from django.core.exceptions import ImproperlyConfigured
 
 
-def get_secret(secret_name: str, region_name: str) -> dict:
-    """Gets a text secret from AWS Secrets Manager. This function currently doesn't handle binary secrets.
+def get_secret(secret_name: str, region_name: str) -> str:
+    """Gets a secret from AWS Secrets Manager.
 
     It attempts to use the instance or task credentials.
 
+    The secret is returned as text, either as the base64 representation if it's a binary secret or as a json
+
     :param secret_name: The name of the secret
     :param region_name: The region where the secret is stored
-    :return: The dictionary representation of the secret
+    :return: The secret
     """
     session = boto3.session.Session(region_name=region_name)
     client = session.client(
@@ -25,5 +26,7 @@ def get_secret(secret_name: str, region_name: str) -> dict:
         raise ImproperlyConfigured("Failed to retrieve secret value.") from e
 
     if "SecretString" in secret_value:
-        return json.loads(secret_value["SecretString"])
-    raise ImproperlyConfigured("The specified secret doesn't contain any text.")
+        return secret_value["SecretString"]
+    elif "SecretBinary" in secret_value:
+        return secret_value["SecretBinary"]
+    raise ImproperlyConfigured("The specified secret is malformed.")
